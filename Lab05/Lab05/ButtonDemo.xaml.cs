f555445e2e0f706dfae86fae7255d0110bd8ef08
Alumno <Alumno@LAB1509-12>
2023-10-06 15:15:36 -0500

    Agregar archivos de proyecto.

---
+++ Lab05/Lab05/ButtonDemo.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab05
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ButtonDemo : ContentPage
+    {
+        public ButtonDemo()
+        {
+            InitializeComponent();
+        }
+        async void OnButtonClicked(object senter, EventArgs args)
+        {
+
+            await label.RelRotateto(360, 1000);
+        }
+    }
+}
